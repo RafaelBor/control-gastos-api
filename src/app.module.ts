96d07ee2ac9cfca2487e6146c4be612739881ed0
RafaelBor <rafaelborbongracia@gmail.com>
2025-08-27 17:09:07 -0700

    fix correcciones de configuracion sendgrid

--- src/app.module.ts
+++ src/app.module.ts
@@ -42,7 +42,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
         },
       },
       defaults: {
-        from: '"MiauHORROS" <soporte@@miauhorros.com>',
+        from: '"MiauHORROS" <soporte@miauhorros.com>',
       },
     }),
     AuthModule,
